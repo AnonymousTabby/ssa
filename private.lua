49414201985a7543eafbff2fc1ee00dcba95b4e2
AnonymousTabby <ptnowners@outlook.com>
2024-09-30 16:06:59 +0200

    Add files via upload

---
+++ private.lua
@@ -0,0 +1,4 @@
+_G.title = "Atlas Private v0.10"
+_G.debug = true
+_G.uicolour = Color3.fromRGB(37, 0, 66)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chris8889/atlasbss/main/test.lua"))()
